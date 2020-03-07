f85af0fdc2912dbcb909dada19480bf179fba7e5
Andrey Minin <ewkjf@mail.ru>
2020-03-07 19:45:44 +0300

    Second variant

--- src/main/java/mainpackage/repositories/GoodsRepository.java
+++ src/main/java/mainpackage/repositories/GoodsRepository.java
@@ -3,8 +3,14 @@ package mainpackage.repositories;
 import mainpackage.entities.GoodEntity;
 import org.springframework.data.repository.CrudRepository;
 
+import java.math.BigInteger;
 import java.util.Optional;
 
 public interface GoodsRepository extends CrudRepository<GoodEntity, Long> {
-    Optional<GoodEntity> findByName(String name);
+
+    Optional<GoodEntity> findFirstByCategory(String name);
+    Optional<GoodEntity> findById(Long id);
+    Optional<GoodEntity> findFirstByCode(BigInteger code);
+    Optional<GoodEntity> findFirstByName(String name);
+
 }
